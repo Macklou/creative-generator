d48926d1db069de96479ddeff82779e726ebf55c
CharlotteLouise <150289744+Macklou@users.noreply.github.com>
2023-12-03 18:41:30 +0000

    Updated JS
    Added some fake text to test JS

--- src/script.js
+++ src/script.js
@@ -1,12 +1,22 @@
 function generateCreative(event) {
   event.preventDefault();
 
-  new Typewriter("#poem", {
-    strings: "La tombe dit à la rose",
-    autoStart: true,
-    delay: 1,
-    cursor: "",
-  });
+  let topic = document.querySelector('input[type="text"]').value;
+  let poemElement = document.getElementById("poem");
+
+  let poem = `
+    My life closed twice before its close—
+    It yet remains to see
+    If Immortality unveil
+    A third event to me
+
+    So huge, so hopeless to conceive
+    As these that twice befell.
+    Parting is all we know of heaven,
+    And all we need of hell.
+  `;
+
+  poemElement.innerHTML = poem;
 }
 
 let creativeFormElement = document.querySelector("#creative-generator-form");
