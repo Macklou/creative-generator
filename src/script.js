242fbd70ba33883e6e495874b6f61b429c24975d
CharlotteLouise <150289744+Macklou@users.noreply.github.com>
2023-12-04 11:40:12 +0000

    debug script

--- src/script.js
+++ src/script.js
@@ -6,18 +6,27 @@ function displayCreative(response) {
     cursor: "",
   });
 }
+
 function generateCreative(event) {
   event.preventDefault();
 
-  let instructionsInput = document.querySelector("#instructionsInput");
+  let instructionsInput = document.querySelector(
+    "#creative-generator-form input[type='text']"
+  );
   let userInput = instructionsInput.value.trim();
 
   if (userInput === "") {
+    console.error("User input is empty.");
     return;
   }
-}
-function generateCreative(event) {
-  event.preventDefault();
+
+  let apiKey = "cao1cb110f2cb023f87a82357a1c4tea";
+  let prompt = `User Instructions: Generate a quote about ${userInput}`;
+  let context =
+    "You are an intelligent expert and able to provide quotes from famous people on any subject in basic HTML. Make sure to follow the user prompts";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${encodeURIComponent(
+    prompt
+  )}&context=${encodeURIComponent(context)}&key=${apiKey}`;
 
   axios
     .get(apiURL)
@@ -25,19 +34,6 @@ function generateCreative(event) {
     .catch((error) => {
       console.error("Error fetching data:", error);
     });
-}
-function generateCreative(event) {
-  event.preventDefault();
-
-  let instructionsInput = document.querySelector("#instructionsInput");
-
-  let apiKey = "cao1cb110f2cb023f87a82357a1c4tea";
-  let prompt = `User Instructions: Generate a quote about ${instructionsInput.value}`;
-  let context =
-    "You are an intelligent expert and able to provide quotes from famous people on any subject in basic HTML. Make sure to follow the user prompts";
-  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-  axios.get(apiURL).then(displayCreative);
 }
 
 let creativeFormElement = document.querySelector("#creative-generator-form");
